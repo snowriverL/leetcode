154603f15117588a3d4d81b70787458f5b9c29b3
guofuhao <guofuhao@mrfresh.com>
2021-04-01 17:01:09 +0800

    树的比较

---
+++ src/main/java/com/snowriver/leetcode/tree/SearchTree.java
@@ -0,0 +1,69 @@
+package com.snowriver.leetcode.tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @see {
+ *     两个二叉树相同，当且仅当两个二叉树的结构完全相同，且所有对应节点的值相同。因此，可以通过搜索的方式判断两个二叉树是否相同。
+ * }
+ * @author guofuhao 
+ * Created on 2021/4/1 4:54 下午
+ * Email is guofuhao@mrfresh.com
+ * Copyright is 北京滴哩科技有限公司
+ */
+public class SearchTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue1.offer(p);
+        queue2.offer(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            TreeNode left1 = node1.left, right1 = node1.right, left2 = node2.left, right2 = node2.right;
+            if (left1 == null ^ left2 == null) {
+                return false;
+            }
+            if (right1 == null ^ right2 == null) {
+                return false;
+            }
+            if (left1 != null) {
+                queue1.offer(left1);
+            }
+            if (right1 != null) {
+                queue1.offer(right1);
+            }
+            if (left2 != null) {
+                queue2.offer(left2);
+            }
+            if (right2 != null) {
+                queue2.offer(right2);
+            }
+        }
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+    
+}
